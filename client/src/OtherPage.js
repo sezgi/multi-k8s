5e3942361ddb427e742a4fbadee4a0ab8aaac86b
sezgi <sezgi.ulucam@gmail.com>
2020-08-29 14:16:26 -0700

    initial commit

---
+++ client/src/OtherPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default () => {
+  return (
+    <div>
+      I'm some otter page 🦦
+      <Link to="/">Go back home</Link>
+    </div>
+  );
+};
